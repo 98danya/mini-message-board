0664a51be34a50c9012a75310d44683ecc06c2c1
98.danya <1998.danyamohammed@gmail.com>
2025-03-18 04:10:02 +0100

    Updated .env db details

--- db/pool.js
+++ db/pool.js
@@ -2,28 +2,14 @@ require('dotenv').config();
 const { Pool } = require('pg');
 
 const pool = new Pool({
-  user: process.env.DATABASE_USER,
-  host: process.env.DATABASE_HOST,
-  database: process.env.DATABASE_NAME,
-  password: process.env.DATABASE_PASSWORD,
-  port: process.env.DATABASE_PORT || 5432,  
+  connectionString: process.env.DATABASE_URL,
   ssl: {
-    rejectUnauthorized: false,
-    sslmode: 'require',
+    rejectUnauthorized: false
   }
 });
 
 pool.connect()
   .then(() => console.log('Connected to PostgreSQL'))
-  .catch((err) => {
-    console.error('Connection error', err.message);
-    console.error('Full error stack:', err.stack);
-  });
-
-console.log(process.env.DATABASE_HOST);
-console.log(process.env.DATABASE_USER);
-console.log(process.env.DATABASE_PASSWORD);
-console.log(process.env.DATABASE_NAME);
-console.log(process.env.DATABASE_PORT);
+  .catch((err) => console.error('Connection error', err.stack));
 
 module.exports = pool;
